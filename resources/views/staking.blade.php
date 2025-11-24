585a2f91b74e0bcbe569897a7431aaf581652311
zainh332 <zain_h332@hotmail.com>
2025-11-24 19:19:53 +0500

    url updated

--- resources/views/staking.blade.php
+++ resources/views/staking.blade.php
@@ -396,7 +396,7 @@
         }
 
         const XAMAN_API_KEY = '8e39c96e-8a2b-4a99-a3f3-383f14480d66';
-        const XAMAN_REDIRECT = 'http://xyield.test/';
+        const XAMAN_REDIRECT = 'https://xyield.vip/';
 
         let xumm = null;
 
